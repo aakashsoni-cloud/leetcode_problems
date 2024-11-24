7416398382c559bd197cf6d937b4f65ecfd0e093
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-11-24 23:05:05 +0530

    Time: 1363 ms (5.11%), Space: 77.6 MB (13.62%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,23 +3,53 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
+
+    // OPTIMAL O(n)
     if (nums.length === 0) return 0;
 
     let numSet = new Set(nums);
     let longest = 0;
 
+    console.log(numSet)
+
     for (const num of nums) {
-        if (numSet.has(num + 1)) continue;
+        if (numSet.has(num - 1)) continue;
+
+        console.log("123");
 
         let counter = 1;
         let currNum = num;
 
-        while (numSet.has(--currNum)) {
+        while (numSet.has(++currNum)) {
             counter++;
         }
         longest = Math.max(longest, counter)
     }
 
-
     return longest;
+    
+
+    /* 
+    BETTER O(nlogn + n)
+    let longest = 1;
+    let counter = 0;
+    let lastSmaller = Number.MIN_VALUE;
+
+    nums.sort((a,b) => a-b);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] - 1 == lastSmaller) {
+            counter += 1;
+            lastSmaller = nums[i];
+        }
+        else if (lastSmaller != nums[i]) {
+            counter = 1;
+            lastSmaller = nums[i];
+        }
+
+        longest = Math.max(longest, counter);
+    }
+    return longest;
+
+    */
 };
