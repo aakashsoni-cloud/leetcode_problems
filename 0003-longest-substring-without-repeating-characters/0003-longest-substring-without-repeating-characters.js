d92b0d00077d928635ebc3f9c715da16259eb611
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-11-24 13:09:09 +0530

    Time: 32 ms (24.53%), Space: 56.7 MB (26.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,6 +4,7 @@
  */
 var lengthOfLongestSubstring = function (s) {
     // sliding window and set
+    /*
     let set = new Set()
     let left = 0;
     let maxSize = 0;
@@ -16,6 +17,25 @@ var lengthOfLongestSubstring = function (s) {
         set.add(s[right]);
         maxSize = Math.max(maxSize, right - left + 1);
     }
-    
+    */
+
+    // SLiding Window and Hashing
+
+    let output = {};
+    let left = 0;
+
+    let maxSize = 0;
+
+    for (let rigth = 0; rigth < s.length; rigth++) {
+        output[s[rigth]] = (output[s[rigth]] || 0) + 1;
+
+        while (output[s[rigth]] > 1) {
+            output[s[left]] -= 1;
+            left++;
+        }
+
+        maxSize = Math.max(maxSize, rigth - left + 1)
+    }
+
     return maxSize;
 };
