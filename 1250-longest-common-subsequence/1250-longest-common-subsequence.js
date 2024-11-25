a15cd0dd13288b0271a2a83635ac2e4915b9427b
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-11-25 09:09:56 +0530

    Time: 43 ms (85.89%), Space: 70.9 MB (86.52%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.js
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.js
@@ -5,13 +5,12 @@
  */
 var longestCommonSubsequence = function (text1, text2) {
 
-    let dp = Array.from(Array(text1.length + 1), () =>
-        Array(text2.length + 1).fill(0)
-    );
+    let dp = Array.from(Array(text1.length + 1).fill(0), () => Array(text2.length + 1).fill(0))
 
     for (let i = 1; i <= text1.length; i++) {
         for (let j = 1; j <= text2.length; j++) {
-            if (i < 0 || j < 0) return 0;
+
+
             if (text1[i - 1] === text2[j - 1]) {
                 dp[i][j] = 1 + dp[i - 1][j - 1];
             } else {
