78c78b27685873af80ad9af04623401c5e591059
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-11-25 12:01:41 +0530

    Time: 1 ms (96.04%), Space: 57.2 MB (80.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -5,13 +5,9 @@
 var maxArea = function (height) {
     let left = 0;
     let right = height.length - 1;
-
     let maxArea = 0;
-    let current = 0;
-
     while (left < right) {
         maxArea = Math.max(maxArea, (right - left) * Math.min(height[right], height[left]));
-
         if (height[left] < height[right]) {
             left++;
         } else {
