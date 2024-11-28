adefffad042690e2a672ed2f10d2c6e05b0b90bf
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-11-28 12:05:48 +0530

    Time: 144 ms (76.19%), Space: 73.8 MB (86.8%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -2,20 +2,45 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindromeSubseq = function (s1) {
-    let s2 = s1.split("").reverse().join("");
-    let dp = Array.from(Array(s1.length + 1).fill(0), () =>
-        Array(s2.length + 1).fill(0)
+var longestPalindromeSubseq = function (s) {
+    let t = s.split("").reverse().join("");
+    let dp = Array.from(Array(s.length + 1).fill(0), () =>
+        Array(t.length + 1).fill(0)
     );
 
-    for (let i = 1; i <= s1.length; i++) {
-        for (let j = 1; j <= s2.length; j++) {
-            if (s1[i - 1] === s2[j - 1]) {
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 1; j <= t.length; j++) {
+            if (s[i - 1] === t[j - 1]) {
                 dp[i][j] = 1 + dp[i - 1][j - 1];
             } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
             }
         }
     }
-    return dp[s1.length][s2.length];
+
+    return dp[s.length][t.length];
+
+    // let ans = [];
+    // let len = dp[s.length][t.length];
+    // let max = 0
+    // let index = len - 1;
+    // let i = s.length - 1;
+    // let j = t.length - 1;
+
+    // while (i >= 0 && j >= 0) {
+    //     if (s[i - 1] == t[j - 1]) {
+    //         ans[index] = s[i - 1];
+    //         i--;
+    //         j--;
+    //         index--;
+    //     } else if (dp[i - 1][j] > dp[i][j - 1]) {
+    //         i--;
+    //     } else {
+    //         j--
+    //     }
+    // }
+
+    // return ans;
+
 };
