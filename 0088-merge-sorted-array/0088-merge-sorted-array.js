215c078b8f1c0ba33311bcc4e743f30da8123e58
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-11-28 10:05:44 +0530

    Time: 0 ms (100%), Space: 49.3 MB (31.67%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    let temp = [];
+    let first = m - 1;
+    let second = n - 1
+    let alength = m + n - 1;
+    if (n == 0) {
+        nums1 = nums1;
+    } else {
+        while (second >= 0) {
+            if (nums1[first] >= nums2[second] && first >= 0) {
+                nums1[alength] = nums1[first];
+                first--;
+                alength--;
+            } else {
+                nums1[alength] = nums2[second];
+                second--;
+                alength--;
+            }
+        }
+    }
+
+};
