2e87da8db0542a93a6ba8226f0b1053127469b46
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-04 00:11:10 +0530

    Time: 5 ms (100%), Space: 50.5 MB (77.44%) - LeetHub

---
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function (s) {
+    // s = s.split("");
+    let lastSeen = [-1, -1, -1];
+    let cnt = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        lastSeen[s.charCodeAt(i) - 'a'.charCodeAt(0)] = i;
+
+        if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+            let min1 = Math.min(lastSeen[0], lastSeen[1]);
+            let min2 = Math.min(lastSeen[1], lastSeen[2]);
+            let res = min1;
+            if (min1 > min2) {
+                res = min2;
+            }
+            cnt = cnt + (1 + res)
+        }
+    }
+    return cnt;
+
+};
