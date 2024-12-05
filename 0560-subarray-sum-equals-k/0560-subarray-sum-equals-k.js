c296879f78cdb30fd24a7a96530957d0b6998a8a
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-05 11:33:37 +0530

    Time: 43 ms (29.19%), Space: 61.5 MB (21.45%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -6,6 +6,7 @@
 var subarraySum = function (nums, k) {
 
     // Better TC: O(N^2);
+    /*
     let max = 0;
     for (let i = 0; i < nums.length; i++) {
         let sum = 0;
@@ -18,4 +19,25 @@ var subarraySum = function (nums, k) {
         }
     }
     return max
+    */
+
+
+    // Optimsed TC: O(N)
+    let prefixMap = {};
+    prefixMap[0] = 1;
+    let prefixSum = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        prefixSum += nums[i];
+
+        let remove = prefixSum - k;
+
+        if (prefixMap[remove] !== undefined) {
+            count += prefixMap[remove];
+        }
+        prefixMap[prefixSum] = (prefixMap[prefixSum] || 0) + 1;
+    }
+
+    return count
 };
