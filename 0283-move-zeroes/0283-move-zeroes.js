34fb9cc21de3753b9285426688c45953cc5f11fd
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-09 14:46:10 +0530

    Time: 1 ms (79.59%), Space: 55.4 MB (12.69%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let n = nums.length;
+    let i = 0;
+    for (let j = 0; j < n; j++) {
+        if (nums[j] !== 0) {
+            let temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            i++;
+        }
+    }
+
+    return nums;
+};
