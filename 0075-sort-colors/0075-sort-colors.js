77111d83b17104292372f232f4dad5916ad71c21
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-09 23:33:48 +0530

    Time: 7 ms (5.9%), Space: 50.7 MB (9.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -4,11 +4,23 @@
  */
 var sortColors = function (nums) {
     let i = 1, n = nums.length
-    nums = nums.sort((a, b) => a - b)
-    // for (let j = 1; j < n; j++) {
-    //     if (nums[j] < nums[i - 1]) {
-    //         [nums[j], nums[i - 1]] = [nums[i - 1], nums[j]]
-    //         i++;
-    //     }
-    // }
+    // nums = nums.sort((a, b) => a - b)
+
+    let colors = { 0: 0, 1: 0, 2: 0 };
+
+    for (let i = 0; i < n; i++) {
+        colors[nums[i]]++;
+    }
+
+    console.log(colors)
+
+    let ind = 0;
+    for (let color = 0; color < 3; color++) {
+        let freq = colors[color];
+        for (let k = 0; k < freq; k++) {
+            nums[ind] = color;
+            ind++;
+        }
+    }
+
 };
