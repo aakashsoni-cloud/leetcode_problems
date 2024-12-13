c513ea059d2916fd904d51ceee9613d2cb69e3c4
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-13 14:05:58 +0530

    Time: 2 ms (91.04%), Space: 52.2 MB (76.21%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.js
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.js
@@ -3,42 +3,13 @@
  * @param {number} goal
  * @return {number}
  */
-var numSubarraysWithSum = function (nums, goal) {
-    // TC: O(N) SC: O(N);
-    // PREFIX SUM
-    /*
-    
-    let prefixSum = 0;
-   let count = 0;
-   const map = new Map();
-   map.set(0, 1);
-
-
-   for (let i = 0; i < nums.length; i++) {
-       prefixSum += nums[i];
-       let remove = prefixSum - goal;
-       count += map.get(remove) || 0;
-       map.set(prefixSum, (map.get(prefixSum) || 0) + 1);
-   }
-   return count;
-
-   */
-
-
-    // TWO POINTER 
-    // TC: O(2N) SC: O(1);
-
-    return atmostK(nums, goal) - atmostK(nums, goal - 1);
-};
-
-var atmostK = function (nums, goal) {
+function subArray(nums, goal) {
     let l = 0;
     let r = 0;
-    let sum = 0;
     let count = 0;
+    let sum = 0;
 
-    if (goal < 0) return 0;
-    while (r <= nums.length - 1) {
+    while (r < nums.length) {
         sum += nums[r];
 
         while (l <= r && sum > goal) {
@@ -49,6 +20,8 @@ var atmostK = function (nums, goal) {
         count = count + (r - l + 1);
         r++;
     }
-
     return count;
+}
+function numSubarraysWithSum(nums, goal) {
+    return subArray(nums, goal) - subArray(nums, goal - 1);
 }
