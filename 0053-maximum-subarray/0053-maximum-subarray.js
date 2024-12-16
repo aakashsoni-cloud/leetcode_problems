13c2da3f8797d565a46e681139d9dbcaed122d4e
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-16 11:09:19 +0530

    Time: 4 ms (20.2%), Space: 58.8 MB (19.4%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    // Brute force O(N^2)
+
+    // let maxSum = nums[0];
+    // for (let i = 0; i < nums.length; i++) {
+    //     let sum = 0;
+
+    //     for (let j = i; j < nums.length; j++) {
+    //         sum += nums[j];
+    //         if (sum > maxSum) {
+    //             maxSum = sum;
+    //         }
+    //     }
+    // }
+    // return maxSum;
+
+    // Optimised 
+
+    let maxSum = nums[0];
+    let sum = 0;
+    let r = 0;
+    while (r < nums.length) {
+        sum += nums[r];
+        maxSum = Math.max(maxSum, sum)
+        if (sum < 0) {
+            sum = 0;
+        }
+        r++;
+    }
+
+    console.log(maxSum);
+
+    return maxSum;
+};
