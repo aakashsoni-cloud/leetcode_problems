b8c8ecac3129eabfa3f4a1984172960680d878ff
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-18 09:06:42 +0530

    Time: 20 ms (43.56%), Space: 52.7 MB (57.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,12 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
+    let letters = {};
     if (s.length !== t.length) return false;
-    let letters = {}
+
     for (let i = 0; i < s.length; i++) {
         letters[s[i]] = letters[s[i]] ? letters[s[i]] + 1 : 1;
-        letters[t[i]] = letters[t[i]] ? letters[t[i]] - 1 : -1;
+        letters[t[i]] = letters[t[i]] ? letters[t[i]] - 1 : - 1;
     }
+
+    console.log(letters);
 
     for (let letter in letters) {
         if (letters[letter] !== 0) {
