b90d571e3d3b16d1f8ee22a2d5466dea77f475da
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-19 23:55:22 +0530

    Time: 2696 ms (5.03%), Space: 51.8 MB (92.94%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,40 +4,22 @@
  * @return {number}
  */
 var subarraySum = function (nums, k) {
+    let maxLength = 0;
+    let sum;
+    let count = 0;
+    let n = nums.length;
 
-    // Better TC: O(N^2);
-    /*
-    let max = 0;
-    for (let i = 0; i < nums.length; i++) {
-        let sum = 0;
-        for (let j = i; j < nums.length; j++) {
+    for (let i = 0; i < n; i++) {
+        sum = 0;
+        for (let j = i; j < n; j++) {
             sum += nums[j];
 
-            if (sum == k) {
-                max++;
+            if (sum === k) {
+                count++;
             }
+            maxLength = Math.max(maxLength, count)
         }
     }
-    return max
-    */
 
-
-    // Optimsed TC: O(N)
-    let prefixMap = {};
-    prefixMap[0] = 1;
-    let prefixSum = 0;
-    let count = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        prefixSum += nums[i];
-
-        let remove = prefixSum - k;
-
-        if (prefixMap[remove] !== undefined) {
-            count += prefixMap[remove];
-        }
-        prefixMap[prefixSum] = (prefixMap[prefixSum] || 0) + 1;
-    }
-
-    return count
+    return maxLength;
 };
