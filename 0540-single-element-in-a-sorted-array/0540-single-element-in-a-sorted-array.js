78ad09821f9f5c55b7ae4a74d7b2a5ac80a2605d
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-27 21:18:07 +0530

    Time: 0 ms (100%), Space: 52.1 MB (78.97%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -4,17 +4,17 @@
  */
 var singleNonDuplicate = function (arr) {
     // Approch 1 Linear Search TC O(N)
-
-    let n = arr.length - 1;
+    /*
+    let n = arr.length;
     if (n === 1) return arr[0];
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= n - 1; i++) {
         if (i == 0) {
             if (arr[i] !== arr[i + 1]) {
                 return arr[i];
             }
         }
         else if (i == n) {
-            if (arr[n] !== arr[n - 1]) {
+            if (arr[n - 1] !== arr[n - 2]) {
                 return arr[i];
             }
         }
@@ -22,6 +22,37 @@ var singleNonDuplicate = function (arr) {
             if (arr[i] !== arr[i - 1] && arr[i] !== arr[i + 1]) {
                 return arr[i]
             }
+        }
+    }
+    */
+
+    // Approch 2 Binary Search
+
+    let n = arr.length;
+
+    if (n === 1) return arr[0];
+    // take first and last condition out first
+    if (arr[0] !== arr[1]) return arr[0];
+    if (arr[n - 1] !== arr[n - 2]) return arr[n - 1];
+
+    let low = 0;
+    let high = n - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] !== arr[mid + 1] && arr[mid] !== arr[mid - 1]) {
+            return arr[mid]
+        }
+        // (even, odd) check for both -> eleminate the right half
+        // at odd and check for left or at even check for right
+        if ((mid % 2 == 1 && arr[mid] !== arr[mid - 1]) || (mid % 2 == 0 && arr[mid] !== arr[mid + 1])) {
+            high = mid - 1;
+        }
+        // (odd, even) check for both -> eleminate the left half 
+        // at odd and check for right or at even check for left
+        if ((mid % 2 == 1 && arr[mid] !== arr[mid + 1]) || (mid % 2 == 0 && arr[mid] !== arr[mid - 1])) {
+            low = mid + 1;
         }
     }
 };
