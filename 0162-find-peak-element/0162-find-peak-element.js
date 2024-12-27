fdcbc48918406ac02bc7a5f791fb3c70dee9ffc8
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-28 00:20:40 +0530

    Time: 0 ms (100%), Space: 48.8 MB (76.96%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -31,8 +31,7 @@ var findPeakElement = function (arr) {
             return mid;
         }
         else if (arr[mid] > arr[mid - 1]) low = mid + 1;
-        else if (arr[mid] > arr[mid + 1]) high = mid - 1;
-        else low = mid + 1;
+        else high = mid - 1;
     }
     return -1;
 };
