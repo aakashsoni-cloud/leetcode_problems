fcfb1c38a0e3bc1a4d70377fb8dd447589630037
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-28 00:20:12 +0530

    Time: 0 ms (100%), Space: 49.2 MB (43.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,11 +3,36 @@
  * @return {number}
  */
 var findPeakElement = function (arr) {
-    // Linear Search 
+    // Linear Search
+    /* 
     let n = arr.length;
     for (let i = 0; i <= n - 1; i++) {
-        if ((i == 0 || arr[i - 1] < arr[i]) && (i == n - 1 || arr[i] > arr[i + 1])) {
+        if ((i == 0 || arr[i - 1] < arr[i]) && (i == n-1  ||arr[i] > arr[i + 1])) {
             return i;
         }
     }
+    */
+
+    // Binary Seearch 
+
+    let n = arr.length;
+
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+    let low = 1;
+    let high = n - 2;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+        else if (arr[mid] > arr[mid - 1]) low = mid + 1;
+        else if (arr[mid] > arr[mid + 1]) high = mid - 1;
+        else low = mid + 1;
+    }
+    return -1;
 };
