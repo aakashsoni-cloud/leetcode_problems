9cc2937d8aaa04f18349472e69e77dc075b65535
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2024-12-28 00:02:28 +0530

    Time: 0 ms (100%), Space: 52.4 MB (56.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -35,8 +35,8 @@ var singleNonDuplicate = function (arr) {
     if (arr[0] !== arr[1]) return arr[0];
     if (arr[n - 1] !== arr[n - 2]) return arr[n - 1];
 
-    let low = 0;
-    let high = n - 1;
+    let low = 1;
+    let high = n - 2;
 
     while (low <= high) {
         let mid = Math.floor((low + high) / 2);
