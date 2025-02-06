68201a736fcebc2ab8cab5d23bcb61395e9a4a4d
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-06 12:04:32 +0530

    Time: 0 ms (100%), Space: 54.4 MB (50.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,13 +2,18 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-    let i = 0;
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] !== 0) {
-            [nums[i], nums[j]] = [nums[j], nums[i]];
-            i++;
-        }
+var moveZeroes = function(arr) {
+    let l = 0;
+  let r = 0;
+
+  while (r < arr.length) {
+    if (arr[r] !== 0) {
+      [arr[l], arr[r]] = [arr[r], arr[l]];
+      l++;
+      r++;
+    } else {
+      r++;
     }
-    return nums;
+  }
+  return arr;
 };
