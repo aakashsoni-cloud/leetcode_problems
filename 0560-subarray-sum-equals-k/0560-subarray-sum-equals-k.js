1749f81bbc71d6a031a4115cd3f3ca2173996a27
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-10 00:20:57 +0530

    Time: 35 ms (43.98%), Space: 61.6 MB (21.09%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -3,20 +3,21 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function (nums, k) {
+var subarraySum = function (num, k) {
     let prefixSum = 0;
     let prefixMap = {};
     prefixMap[0] = 1;
 
     let count = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        prefixSum += nums[i]
+    for (let i = 0; i < num.length; i++) {
+        prefixSum += num[i];
 
         let remove = prefixSum - k;
+
         count += prefixMap[remove] || 0;
 
-        prefixMap[prefixSum] = (prefixMap[prefixSum] || 0) + 1
+        prefixMap[prefixSum] = (prefixMap[prefixSum] || 0) + 1;
     }
 
     return count;
