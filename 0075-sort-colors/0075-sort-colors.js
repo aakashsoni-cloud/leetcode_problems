90a303ecc21f4b9e5a59de1d878d4b69dc1aee49
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-13 17:26:31 +0530

    Time: 0 ms (100%), Space: 50.5 MB (13.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,25 +2,43 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function (nums) {
-    let n = nums.length;
-    let count0 = 0;
-    let count1 = 0;
-    let count2 = 0;
+var sortColors = function (arr) {
+    let n = arr.length;
+    // let count0 = 0;
+    // let count1 = 0;
+    // let count2 = 0;
 
-    for (let i = 0; i < n; i++) {
-        if (nums[i] == 0) count0++
-        else if (nums[i] == 1) count1++;
-        else count2++;
-    }
+    // for (let i = 0; i < n; i++) {
+    //     if (nums[i] == 0) count0++
+    //     else if (nums[i] == 1) count1++;
+    //     else count2++;
+    // }
 
-    for(let i=0;i<count0;i++) {
-        nums[i] = 0;
-    }
-    for(let i=count0;i<count0+count1;i++) {
-        nums[i] = 1;
-    }
-    for(let i=count0+count1;i<count0+count1+count2;i++) {
-        nums[i] = 2;
+    // for(let i=0;i<count0;i++) {
+    //     nums[i] = 0;
+    // }
+    // for(let i=count0;i<count0+count1;i++) {
+    //     nums[i] = 1;
+    // }
+    // for(let i=count0+count1;i<count0+count1+count2;i++) {
+    //     nums[i] = 2;
+    // }
+
+
+    let low = 0;
+    let mid = 0;
+    let high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            [arr[low], arr[mid]] = [arr[mid], arr[low]]
+            low++;
+            mid++
+        } else if (arr[mid] == 1) {
+            mid++;
+        } else {
+            [arr[mid], arr[high]] = [arr[high], arr[mid]];
+            high--;
+        }
     }
 };
