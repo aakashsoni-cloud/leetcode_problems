4408863b9e0eb28d9d7df48ca1bb11ddfb159ac0
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-13 16:46:50 +0530

    Time: 0 ms (100%), Space: 49.1 MB (61.48%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,24 +3,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    let i = 1, n = nums.length
-    // nums = nums.sort((a, b) => a - b)
-
-    let colors = { 0: 0, 1: 0, 2: 0 };
+    let n = nums.length;
+    let count0 = 0;
+    let count1 = 0;
+    let count2 = 0;
 
     for (let i = 0; i < n; i++) {
-        colors[nums[i]]++;
+        if (nums[i] == 0) count0++
+        else if (nums[i] == 1) count1++;
+        else count2++;
     }
 
-    console.log(colors)
-
-    let ind = 0;
-    for (let color = 0; color < 3; color++) {
-        let freq = colors[color];
-        for (let k = 0; k < freq; k++) {
-            nums[ind] = color;
-            ind++;
-        }
+    for(let i=0;i<count0;i++) {
+        nums[i] = 0;
     }
-
+    for(let i=count0;i<count0+count1;i++) {
+        nums[i] = 1;
+    }
+    for(let i=count0+count1;i<count0+count1+count2;i++) {
+        nums[i] = 2;
+    }
 };
