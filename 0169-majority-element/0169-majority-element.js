d15c8c45d1f9fa758bce755906b78061e37ccad5
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-15 13:21:57 +0530

    Time: 2 ms (81.32%), Space: 51.5 MB (82.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -31,16 +31,42 @@ var majorityElement = function (nums) {
 
     // Hashing
 
-    let map = {};
+    // let map = {};
+    // let n = nums.length;
+
+    // for (let i = 0; i < n; i++) {
+    //     map[nums[i]] = (map[nums[i]] || 0) + 1
+    // }
+
+    // for (let key of Object.keys(map)) {
+    //     if (map[key] > Math.floor(n / 2)) {
+    //         return parseInt(key)
+    //     }
+    // }
+
+    // Moore's Vooting Alogo
+
     let n = nums.length;
+    let count = 0;
+    let elem;
 
     for (let i = 0; i < n; i++) {
-        map[nums[i]] = (map[nums[i]] || 0) + 1
+        if (count == 0) {
+            count = 1;
+            elem = nums[i];
+        } else if (elem == nums[i]) {
+            count++;
+        } else {
+            count--;
+        }
     }
 
-    for (let key of Object.keys(map)) {
-        if (map[key] > Math.floor(n / 2)) {
-            return parseInt(key)
-        }
+    let count1 = 0;
+    for (let i = 0; i < n; i++) {
+        if (elem == nums[i]) count1++;
+    }
+
+    if (count1 > Math.floor(n / 2)) {
+        return elem;
     }
 };
