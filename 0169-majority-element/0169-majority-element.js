3332b4d702ce586f05fcafed7f995215036cde13
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-15 13:06:29 +0530

    Time: 5 ms (51.49%), Space: 51.7 MB (70%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,27 +3,44 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    nums.sort((a, b) => a - b);
+    // nums.sort((a, b) => a - b);// O(nlogn)
+    // let n = nums.length;
+    // let l = 0;
+    // let r = 0;
+
+    // let count = 0;
+
+    // while (r <= n) { // O(N)
+    //     let item = nums[l];
+    //     if (item == nums[r]) {
+    //         count++;
+    //         r++;
+    //     }
+
+    //     if (item !== nums[r]) {
+    //         l = r;
+    //         count = 0;
+    //     }
+
+    //     if (count > Math.floor(n / 2)) {
+    //         return item;
+    //     }
+
+    // }
+
+
+    // Hashing
+
+    let map = {};
     let n = nums.length;
-    let l = 0;
-    let r = 0;
 
-    let count = 0;
+    for (let i = 0; i < n; i++) {
+        map[nums[i]] = (map[nums[i]] || 0) + 1
+    }
 
-    while (r <= n) {
-        let item = nums[l];
-        if (item == nums[r]) {
-            count++;
-            r++;
-        }
-
-        if (count > Math.floor(n / 2)) {
-            return item;
-        }
-
-        if (nums[l] !== nums[r]) {
-            l = r;
-            count = 0;
+    for (let key of Object.keys(map)) {
+        if (map[key] > Math.floor(n / 2)) {
+            return parseInt(key)
         }
     }
 };
