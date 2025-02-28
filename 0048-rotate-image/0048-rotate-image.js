cefeafb07181193657f4793bad41e3489ebd6c4e
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-02-28 10:50:41 +0530

    Time: 0 ms (100%), Space: 54.5 MB (6.24%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -7,7 +7,7 @@ var rotate = function (matrix) {
     let n = matrix.length;
     let m = matrix[0].length;
 
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < m; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         }
