d4f9f53db715aa2ab2adc686aa39f5fbeb03914a
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-02 11:19:59 +0530

    Time: 0 ms (100%), Space: 53 MB (16.15%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,20 +3,32 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    let count = 0;
-    let step = false;
-    for (let c of s) {
-        if (c !== ' ') {
-            if (!step) {
-                step = true;
-                count = 1;
-            } else {
-                count++
-            }
-        } else {
-            step = false;
-        }
+    // let count = 0;
+    // let step = false;
+    // for (let c of s) {
+    //     if (c !== ' ') {
+    //         if (!step) {
+    //             step = true;
+    //             count = 1;
+    //         } else {
+    //             count++
+    //         }
+    //     } else {
+    //         step = false;
+    //     }
+    // }
+
+    // return count;
+
+    let end = s.length - 1;
+
+    while (end >= 0 && s[end] == ' ') {
+        end--;
     }
 
-    return count;
+    let start = end;
+    while (start >= 0 && s[start] !== ' ') {
+        start--;
+    }
+    return end - start
 };
