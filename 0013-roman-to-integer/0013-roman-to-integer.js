2916a8908777d4a48a1bf09926ec3f7240a936d1
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-04 00:20:52 +0530

    Time: 6 ms (61.23%), Space: 61.1 MB (9.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -8,21 +8,11 @@ var romanToInt = function (s) {
     let sum = 0;
 
     for (let i = 0; i < s.length; i++) {
-
-        if ((s[i + 1] == 'V' || s[i + 1] == 'X') && s[i] == 'I') {
-            sum += (romanNumber[s[i + 1]] - romanNumber[s[i]]);
-            i += 1
-        } else if ((s[i + 1] == 'L' || s[i + 1] == 'C') && s[i] == 'X') {
-            sum += (romanNumber[s[i + 1]] - romanNumber[s[i]]);
-            i += 1
-        } else if ((s[i + 1] == 'D' || s[i + 1] == 'M') && s[i] == 'C') {
-            sum += (romanNumber[s[i + 1]] - romanNumber[s[i]]);
-            i += 1;
-        } else if (s[i] == 'I' || s[i] == 'V' || s[i] == 'X' || s[i] == 'L' || s[i] == 'C' || s[i] == 'D' || s[i] == 'M') {
+        if (romanNumber[s[i]] < romanNumber[s[i + 1]]) {
+            sum -= romanNumber[s[i]]
+        } else {
             sum += romanNumber[s[i]];
         }
-        console.log(sum)
-
     }
 
     return sum;
