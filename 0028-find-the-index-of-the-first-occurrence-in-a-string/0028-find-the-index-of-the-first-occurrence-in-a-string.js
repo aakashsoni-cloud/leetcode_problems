28da18e6f7e4aaa6e7245d4c21ee631fe598d16b
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-06 15:10:33 +0530

    Time: 0 ms (100%), Space: 53.1 MB (23.36%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,13 +4,5 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-    let split = haystack.split(needle);
-
-    console.log(split)
-
-    if (split.length == 1) {
-        return -1;
-    }
-
-    return split[0].length;
+    return haystack.indexOf(needle)
 };
