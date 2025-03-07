bf0b90953053b8c30347db91ab4a56602f2d42ee
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-08 00:56:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -2,36 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function (arr) {
-    // Linear Search
-    /* 
-    let n = arr.length;
-    for (let i = 0; i <= n - 1; i++) {
-        if ((i == 0 || arr[i - 1] < arr[i]) && (i == n-1  ||arr[i] > arr[i + 1])) {
-            return i;
-        }
-    }
-    */
+var findPeakElement = function (nums) {
+    let n = nums.length;
+    let low = 0;
+    let high = n - 1;
 
-    // Binary Seearch 
-
-    let n = arr.length;
-
-    if (n == 1) return 0;
-    if (arr[0] > arr[1]) return 0;
-    if (arr[n - 1] > arr[n - 2]) return n - 1;
-
-    let low = 1;
-    let high = n - 2;
 
     while (low <= high) {
         let mid = Math.floor((low + high) / 2);
 
-        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
-            return mid;
+        if (nums[mid - 1] < nums[mid] && nums[mid + 1] < nums[mid]) {
+            return mid; // peak index
         }
-        else if (arr[mid] > arr[mid - 1]) low = mid + 1;
-        else high = mid - 1;
+
+        if (nums[mid - 1] < nums[mid] && nums[mid] < nums[mid + 1]) {
+            if (nums[low] < nums[mid]) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        } else {
+            high = mid - 1;
+        }
     }
-    return -1;
 };
