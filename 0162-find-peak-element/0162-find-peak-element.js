9b7d97d3f5bda1c553a7af5dcdbd581d4c32feb3
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-08 01:01:37 +0530

    Time: 0 ms (100%), Space: 53 MB (23.62%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -4,8 +4,13 @@
  */
 var findPeakElement = function (nums) {
     let n = nums.length;
-    let low = 0;
-    let high = n - 1;
+
+    if (n == 1) return 0;
+    if (nums[0] > nums[1]) return 0;
+    if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+    let low = 1;
+    let high = n - 2;
 
 
     while (low <= high) {
@@ -13,16 +18,11 @@ var findPeakElement = function (nums) {
 
         if (nums[mid - 1] < nums[mid] && nums[mid + 1] < nums[mid]) {
             return mid; // peak index
-        }
-
-        if (nums[mid - 1] < nums[mid] && nums[mid] < nums[mid + 1]) {
-            if (nums[low] < nums[mid]) {
-                low = mid + 1
-            } else {
-                high = mid - 1
-            }
+        } else if (nums[mid - 1] < nums[mid]) {
+            low = mid + 1
         } else {
             high = mid - 1;
         }
     }
+    return -1;
 };
