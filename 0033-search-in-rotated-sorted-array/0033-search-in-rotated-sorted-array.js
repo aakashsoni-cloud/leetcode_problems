402f519416c32ff192985310492964968bab2398
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-12 17:06:58 +0530

    Time: 0 ms (100%), Space: 54.6 MB (5.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,31 +3,31 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (arr, target) {
+var search = function (nums, target) {
+    let n = nums.length;
     let low = 0;
-    let high = arr.length - 1;
+    let high = n - 1;
 
     while (low <= high) {
         let mid = Math.floor((low + high) / 2);
 
-        if (arr[mid] === target) return mid;
+        if (nums[mid] == target) return mid;
 
-        // left sorted
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= target && target <= arr[mid])
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target <= nums[mid]) {
                 high = mid - 1;
-            else
+            } else {
                 low = mid + 1;
+            }
         }
-
-
-        // rigth sorted
-        if (arr[mid] <= arr[high]) {
-            if (arr[mid] <= target && target <= arr[high])
+        if (nums[mid] <= nums[high]) {
+            if (nums[mid] <= target && target <= nums[high]) {
                 low = mid + 1;
-            else
+            } else {
                 high = mid - 1;
+            }
         }
     }
-    return -1
+
+    return -1;
 };
