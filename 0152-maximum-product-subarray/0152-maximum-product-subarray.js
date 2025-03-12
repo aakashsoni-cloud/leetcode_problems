290e49511032d4a7a57acc467744fd5191bed1ab
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-12 12:08:36 +0530

    Time: 336 ms (5.1%), Space: 54.2 MB (32.13%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+    let n = nums.length;
+    let currPro = 1;
+    let maxPro = -1;
+
+    if (n == 1) return nums[0]
+
+    for (let i = 0; i < n; i++) {
+        currPro = nums[i];
+        maxPro = Math.max(maxPro, currPro);
+
+        for (let j = i + 1; j < n; j++) {
+            currPro *= nums[j];
+            maxPro = Math.max(maxPro, currPro);
+        }
+    }
+    return maxPro;
+};
