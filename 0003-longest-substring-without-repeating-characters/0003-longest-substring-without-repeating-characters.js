e892af91634934c6f7e80ec7d7cdfeabb63ff246
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-14 18:00:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,24 +3,20 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let hashMap = {};
+    let set = new Set();
+
+    let n = s.length - 1;
     let l = 0;
     let r = 0;
+    let maxLength = 0
 
-    let maxLength = 0;
-
-    if (s.length == 1) return 1;
-    while (r < s.length) {
-        let rightChar = s[r];
-        hashMap[rightChar] = hashMap[rightChar] + 1 || 1;
-
-        while (hashMap[rightChar] > 1) {
-            let leftChar = s[l];
-            hashMap[leftChar]--;
+    while (r < n) {
+        while (set.has(s[r])) {
+            set.delete(s[l]);
             l++;
         }
-        maxLength = Math.max(maxLength, r - l + 1);
-
+        set.add(s[r]);
+        maxLength = Math.max(maxLength, r - l + 1)
         r++;
     }
 
