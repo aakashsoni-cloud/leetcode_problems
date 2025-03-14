846f9619b9712dff6d0ea389c9777155317f4089
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-14 19:56:22 +0530

    Time: 3 ms (50.14%), Space: 60.7 MB (10.21%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -5,17 +5,14 @@
 var findMaxConsecutiveOnes = function (nums) {
     let max = 0;
     let sum = 0;
-
     let r = 0;
+    let l = 0;
 
     while (r < nums.length) {
-        sum += nums[r];
-
         if (nums[r] == 0) {
-            sum = 0;
+            l = r + 1;
         }
-
-        max = Math.max(max, sum);
+        max = Math.max(max, r - l + 1);
         r++;
     }
     return max;
