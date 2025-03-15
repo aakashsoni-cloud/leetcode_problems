dcb10f85a8fa2511aa45bedc7f93cb6ef2e0b9a6
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-15 20:33:40 +0530

    Time: 5 ms (60.5%), Space: 57.5 MB (29.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,12 +3,25 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
+    // s = s.replace(/[:,_-\W]/g, "").toLowerCase();
+    // let n = s.length;
+
+    // let l = 0;
+    // let r = n - 1;
+
+    // while (l <= r) {
+    //     if (s[l] !== s[r]) {
+    //         return false;
+    //     }
+    //     l++;
+    //     r--;
+    // }
+    // return true;
+
     s = s.replace(/[:,_-\W]/g, "").toLowerCase();
     let n = s.length;
 
-    let l = 0;
-    let r = n - 1;
-
+    let l = 0, r = n - 1;
     while (l <= r) {
         if (s[l] !== s[r]) {
             return false;
@@ -16,5 +29,5 @@ var isPalindrome = function (s) {
         l++;
         r--;
     }
-    return true;
+    return true
 };
