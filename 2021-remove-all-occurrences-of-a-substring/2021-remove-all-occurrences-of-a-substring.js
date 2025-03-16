71e63f29f9ab1406abed558cb56dd01a2d4eaa1c
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-16 13:28:39 +0530

    Time: 56 ms (5.21%), Space: 60 MB (5.34%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.js
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.js
@@ -5,11 +5,11 @@
  */
 var removeOccurrences = function (s, part) {
     let newStr
-    let index;
-    while (s.indexOf(part) >= 0) {
-        index = s.indexOf(part);
+    let index = s.indexOf(part);
+    while (index >= 0) {
         console.log(index)
         s = s.substring(0, index) + s.substring(index + part.length);
+        index = s.indexOf(part);
         console.log(s)
     }
     return s;
