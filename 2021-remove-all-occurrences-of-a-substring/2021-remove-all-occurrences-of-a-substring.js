f9106f45a94753cce117fa28f4070d467753dc3b
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-17 23:27:09 +0530

    Time: 6 ms (34.92%), Space: 54.8 MB (19.96%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.js
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.js
@@ -5,17 +5,18 @@
  */
 var removeOccurrences = function (s, part) {
     let stack = [];
-    let targetLength = part.length;
-    let targetEndChar = part[part.length - 1];
+    let partLength = part.length;
+    let partLastEle = part[partLength - 1];
 
     for (let char of s) {
         stack.push(char);
 
-        if (char == targetEndChar && stack.length >= targetLength) {
-            if (stack.slice(-targetLength).join('') == part) {
-                stack.length -= targetLength
+        if (char == partLastEle && stack.length >= partLength) {
+            if (stack.slice(-partLength).join('') == part) {
+                stack.length -= partLength;
             }
         }
     }
+
     return stack.join('')
 };
