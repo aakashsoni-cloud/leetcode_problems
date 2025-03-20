368de9d983bbc5cf727ed9779449f39c1e961370
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-20 19:25:14 +0530

    Time: 7 ms (57.52%), Space: 67.9 MB (49.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,46 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function (num) {
-    // let n = nums.length;
-    // let prod = 1;
-    // let answer = [];
-    // answer[0] = 1;
-
-    // for (let i = 1; i < n; i++) {
-    //     answer[i] = answer[i - 1] * nums[i - 1]
-    // }
-
-    // let suffix = 1;
-
-    // for (let j = n - 2; j >= 0; j--) {
-    //     suffix *= nums[j + 1]
-    //     answer[j] *= suffix;
-    // }
-
-    // return answer;
-
-    let res = [];
-    let prefixPro = [];
-    let suffixPro = [];
-
-    let n = num.length;
-
-    prefixPro[0] = 1;
-    suffixPro[n - 1] = 1;
+var productExceptSelf = function (nums) {
+    let n = nums.length;
+    let result = [];
+    result[0] = 1;
 
     for (let i = 1; i < n; i++) {
-        prefixPro[i] = prefixPro[i - 1] * num[i - 1];
+        result[i] = result[i - 1] * nums[i - 1];
     }
+
     let suffix = 1;
+
     for (let i = n - 2; i >= 0; i--) {
-        suffix *= num[i + 1];
-        suffixPro[i] = suffix;
+        suffix *= nums[i + 1];
+        result[i] *= suffix;
     }
 
-    for (let i = 0; i < n; i++) {
-        res[i] = prefixPro[i] * suffixPro[i];
-    }
-
-    return res;
+    return result;
 };
