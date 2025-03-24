2e9471b67458c3f4bacbad979facdf8e2907629f
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-24 18:35:32 +0530

    Time: 1 ms (64.03%), Space: 57.5 MB (14.26%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var addDigits = function (num) {
+    // num = 154
     if (Math.floor(num / 10) == 0) return num;
-    let rem = Math.floor(num / 10);
-    num = (num % 10) + rem;
+    let pre = Math.floor(num / 10); //  15
+    num = (num % 10) + pre; // (4) + 15
     return addDigits(num);
 };
