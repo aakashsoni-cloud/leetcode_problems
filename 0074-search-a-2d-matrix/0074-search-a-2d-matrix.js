20e70daa8c9f9477aa89a2528a2f5c2e8604d3e1
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-03-26 14:53:30 +0530

    Time: 0 ms (100%), Space: 54.1 MB (15.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,30 +4,20 @@
  * @return {boolean}
  */
 
-function binarySearch(arr, target) {
-    let low = 0;
-    let high = arr.length - 1;
-
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2);
-
-        if (arr[mid] == target) return true;
-        else if (target > arr[mid]) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
-    }
-    return false;
-}
-
 var searchMatrix = function (arr, target) {
     let n = arr.length;
     let m = arr[0].length;
-    for (let i = 0; i < n; i++) {
-        if (arr[i][0] <= target && target <= arr[i][m - 1]) {
-            return binarySearch(arr[i], target);
-        }
+    let low = 0;
+    let high = n * m - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let row = Math.floor(mid / m);
+        let col = mid % m;
+
+        if (arr[row][col] == target) return true;
+        else if (arr[row][col] < target) low = mid + 1;
+        else high = mid - 1;
     }
     return false;
 };
