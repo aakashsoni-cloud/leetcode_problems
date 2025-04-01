823be5edd3b6adadd15b84b06b228935f1cb67dc
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-01 13:26:50 +0530

    Time: 0 ms (100%), Space: 63.7 MB (56.88%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+    let n = arr.length;
+
+    if (arr[0] > arr[1]) return arr[0];
+    if (arr[n - 1] > arr[n - 2]) return arr[n - 1]
+
+    let low = 1;
+    let high = n - 2;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+
+        if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+            low = mid + 1;
+        } else if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1]) {
+            high = mid - 1;
+        } else {
+            high = mid - 1
+        }
+    }
+};
