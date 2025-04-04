b41c9e90147c7903d0f556b9aaee48a43af6e87b
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-05 00:07:12 +0530

    Time: 43 ms (90.84%), Space: 56.6 MB (14.4%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    let sign = x < 0 ? -1 : 1;
+    x = Math.abs(x);
+    let rev = 0;
+
+    while (x !== 0) {
+        const digit = x % 10;
+        x = Math.floor(x / 10);
+
+        if (rev > Math.floor((2 ** 31 - 1) / 10) ||
+            rev === Math.ceil((2 ** 31 - 1) / 10) && digit > 7) {
+            return 0;
+        }
+
+        rev = rev * 10 + digit;
+    }
+    return sign * rev;
+
+    // return sign * parseInt(num.join(""))
+};
