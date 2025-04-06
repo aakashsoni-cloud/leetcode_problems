b2b8a56b738f8f25ee91c4f1b15ceb33961857d9
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-07 00:36:28 +0530

    Time: 2 ms (78%), Space: 55.2 MB (73.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,20 +4,29 @@
  */
 var majorityElement = function (arr) {
     let n = arr.length;
-    arr.sort((a, b) => a - b);
+    let count = 0;
+    let elem;
 
-    if (n == 1) return arr[0]
-    let count = 1;
-    for (let i = 0; i < n - 1; i++) {
-        if (arr[i] == arr[i + 1]) {
-            count++;
-        }
-
-        if (arr[i] != arr[i + 1]) {
+    for (let i = 0; i < n; i++) {
+        if (count == 0) {
             count = 1;
+            elem = arr[i];
+        } else if (elem == arr[i]) {
+            count++;
+        } else {
+            count--;
         }
-        if (count > Math.floor(n / 2)) {
-            return arr[i];
+    }
+
+    let count1 = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (elem == arr[i]) {
+            count1++;
         }
+    }
+
+    if (count1 > Math.floor(n / 2)) {
+        return elem;
     }
 };
