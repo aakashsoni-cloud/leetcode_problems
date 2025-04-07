7cb11d0386c650133f9a3f6ceec1e7ff62ea6635
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-07 10:41:19 +0530

    Time: 5 ms (27.47%), Space: 65.5 MB (40.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -15,9 +15,7 @@ function rotateArray(arr, low, high) {
 
 function rotate(arr, k) {
     let n = arr.length;
-
     k = k % n; // to only rotate the remaining steps 
-
     rotateArray(arr, 0, n - 1)
     rotateArray(arr, 0, k - 1)
     rotateArray(arr, k, n - 1)
