6f2503334a65cbff685014804b8d97a0d3c0b96e
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-07 10:39:34 +0530

    Time: 4 ms (36.72%), Space: 65 MB (50.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -3,23 +3,23 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-function rotateArr(arr, start, end) {
-    while (start <= end) {
-        [arr[start], arr[end]] = [arr[end], arr[start]];
-        start++;
-        end--;
-    }
 
+function rotateArray(arr, low, high) {
+    while (low < high) {
+        [arr[low], arr[high]] = [arr[high], arr[low]]
+        low++;
+        high--;
+    }
     return arr;
 }
 
 function rotate(arr, k) {
     let n = arr.length;
-    k = k % n; // the extra roatate than lenght;
 
-    rotateArr(arr, 0, n - 1);
-    rotateArr(arr, 0, k - 1);
-    rotateArr(arr, k, n - 1);
+    k = k % n; // to only rotate the remaining steps 
 
+    rotateArray(arr, 0, n - 1)
+    rotateArray(arr, 0, k - 1)
+    rotateArray(arr, k, n - 1)
     return arr;
 }
