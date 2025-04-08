7571e1445a5b7712836f136673ae7171ea04d49d
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-08 10:45:58 +0530

    Time: 0 ms (100%), Space: 54.2 MB (44.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let profitSum = 0;
+    let maxProfit = 0;
     let n = prices.length;
 
     let l = 0;
-    let r = 1;
-
+    let r = 0;
     while (r < n) {
         if (prices[r] > prices[l]) {
-            profitSum += prices[r] - prices[l];
+            maxProfit += prices[r] - prices[l];
         }
         l = r;
         r++;
     }
 
-    return profitSum;
+    return maxProfit;
 };
