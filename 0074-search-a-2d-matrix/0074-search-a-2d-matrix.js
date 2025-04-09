5670b8a2cf1384a1f58878ca131d41613fa0ff50
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-09 18:18:23 +0530

    Time: 0 ms (100%), Space: 54.1 MB (21.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -24,13 +24,31 @@ var binarySearch = function (arr, target) {
 }
 var searchMatrix = function (matrix, target) {
     // Brute -> O(N^2) two loops
-    // Better -> Binary with worst time
+    // Better -> Binary with worst time O(N) + O(logM)
+    // Optimise -> Binary Search with optmise the search in row O(log N*M)
     let n = matrix.length;
     let m = matrix[0].length
 
-    for (let i = 0; i < n; i++) {
-        if (matrix[i][0] <= target && matrix[i][m - 1] >= target) {
-            return binarySearch(matrix[i], target);
+    // for (let i = 0; i < n; i++) {
+    //     if (matrix[i][0] <= target && matrix[i][m - 1] >= target) {
+    //         return binarySearch(matrix[i], target);
+    //     }
+    // }
+    // return false;
+    let low = 0;
+    let high = n * m - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        // from hypotetical row and col
+        let row = Math.floor(mid / m), col = mid % m;
+
+        if (matrix[row][col] == target) return true;
+
+        if (matrix[row][col] <= target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1
         }
     }
     return false;
