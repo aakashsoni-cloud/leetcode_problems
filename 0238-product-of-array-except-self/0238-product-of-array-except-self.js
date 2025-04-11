fed70fa93d3568d821923d81efd8814fea32027d
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-11 11:22:37 +0530

    Time: 35 ms (9.81%), Space: 68.5 MB (75.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,19 +4,29 @@
  */
 var productExceptSelf = function (nums) {
     let n = nums.length;
-    let result = [];
-    result[0] = 1;
+    let prefPro = [], sufPro = []
+    prefPro[0] = [1];
+    sufPro[n - 1] = [1];
 
     for (let i = 1; i < n; i++) {
-        result[i] = result[i - 1] * nums[i - 1];
+        prefPro[i] = prefPro[i - 1] * nums[i - 1];
     }
 
-    let suffix = 1;
+
+    let sufProduct = 1;
 
     for (let i = n - 2; i >= 0; i--) {
-        suffix *= nums[i + 1];
-        result[i] *= suffix;
+        sufProduct *= nums[i + 1];
+        sufPro[i] = sufProduct;
+    }
+
+    console.log(sufPro)
+
+    let result = []
+
+    for (let i = 0; i < n; i++) {
+        result[i] = prefPro[i] * sufPro[i]
     }
 
     return result;
-};
+}; 
