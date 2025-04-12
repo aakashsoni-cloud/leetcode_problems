a59dcc425e1616226dce3908f5576b096bde3f29
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-12 22:37:46 +0530

    Time: 6 ms (73.59%), Space: 58.6 MB (57.21%) - LeetHub

--- 0135-candy/0135-candy.js
+++ 0135-candy/0135-candy.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var candy = function (ratings) {
+    // TC: O(2N) SC: O(N)
     let n = ratings.length;
     let leftNeigh = [];
-    let rigthNeigh = [];
 
     leftNeigh[0] = 1;
-    rigthNeigh[n - 1] = 1;
 
     for (let i = 1; i < n; i++) {
         if (ratings[i] > ratings[i - 1]) {
@@ -18,19 +17,17 @@ var candy = function (ratings) {
         }
     }
 
+    let current = 1, right = 1;
+    let sum = Math.max(1, leftNeigh[n - 1]);
     for (let i = n - 2; i >= 0; i--) {
         if (ratings[i] > ratings[i + 1]) {
-            rigthNeigh[i] = rigthNeigh[i + 1] + 1;
+            current += 1;
+            right = current;
         } else {
-            rigthNeigh[i] = 1;
+            current = 1;
         }
-    }
-
-    let sum = 0;
-    for (let i = 0; i < n; i++) {
-        sum += Math.max(leftNeigh[i], rigthNeigh[i])
+        sum = sum + Math.max(leftNeigh[i], current)
     }
 
     return sum;
-
 };
