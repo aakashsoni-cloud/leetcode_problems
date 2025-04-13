afc075e3800a377b3d279b4455142932307b7874
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-13 12:06:41 +0530

    Time: 1 ms (100%), Space: 54 MB (100%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function (x, y, z) {
+    if (Math.abs(z - x) < Math.abs(z - y)) {
+        return 1;
+    } else if (Math.abs(z - x) > Math.abs(z - y)) {
+        return 2;
+    } else {
+        return 0;
+    }
+};
