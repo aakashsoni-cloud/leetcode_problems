f9b42a63aa434b4365c022c0bf17e76dddf6c06f
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-16 12:15:55 +0530

    Time: 52 ms (46.31%), Space: 53.6 MB (52.46%) - LeetHub

--- 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.js
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.js
@@ -5,6 +5,11 @@
  */
 var countOdds = function (low, high) {
     let totalNumbers = (high - low) + 1;
-    let countOdd = Math.floor((high + 1) / 2) - Math.floor(low / 2);
+    let countOdd;
+    if (totalNumbers % 2 !== 0) {
+        countOdd = Math.floor((high + 1) / 2) - Math.floor(low / 2);
+    } else {
+        countOdd = Math.floor(totalNumbers / 2);
+    }
     return countOdd;
 };
