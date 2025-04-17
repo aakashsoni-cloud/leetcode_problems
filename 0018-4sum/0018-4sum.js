cad8f3e0e10a3157de8650acfefcf33d8d69c196
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-17 19:12:35 +0530

    Time: 18 ms (50.53%), Space: 56.4 MB (89%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -8,17 +8,17 @@ var fourSum = function (nums, target) {
 
     let res = [];
 
-    nums.sort((a, b) => a - b);
+    nums.sort((a, b) => a - b)
 
     for (let i = 0; i < n; i++) {
         if (i > 0 && nums[i] == nums[i - 1]) continue;
+
         for (let j = i + 1; j < n; j++) {
 
             if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
             let l = j + 1;
             let r = n - 1;
-
 
             while (l < r) {
                 let sum = nums[i] + nums[j] + nums[l] + nums[r];
@@ -27,15 +27,18 @@ var fourSum = function (nums, target) {
                     res.push([nums[i], nums[j], nums[l], nums[r]])
                     l++;
                     r--;
+
                     while (l > 0 && nums[l] == nums[l - 1]) l++;
-                    while (r > 0 && nums[r] == nums[r + 1]) r--
-                } else if (sum > target) {
-                    r--;
-                } else {
+                    while (r > 0 && nums[r] == nums[r + 1]) r--;
+                } else if (sum < target) {
                     l++;
+                } else {
+                    r--;
                 }
             }
         }
     }
+
     return res;
+
 };
