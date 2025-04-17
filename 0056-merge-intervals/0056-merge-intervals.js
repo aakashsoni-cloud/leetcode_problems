5738d56b6f430e7a951b9dab81b332be015dae83
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-17 18:30:29 +0530

    Time: 7 ms (85.65%), Space: 64.5 MB (68.26%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -5,35 +5,26 @@
 var merge = function (arr) {
     let n = arr.length;
     arr.sort((a, b) => a[0] - b[0]);
-    const res = [];
+    let result = [];
 
     for (let i = 0; i < n; i++) {
-        if (!res.length || arr[i][0] > res[res.length - 1][1]) {
-            res.push(arr[i]);
-        } else {
-            res[res.length - 1][1] = Math.max(res[res.length - 1][1], arr[i][1])
+        let start = arr[i][0];
+        let end = arr[i][1];
+
+        if (result.length && end <= result[result.length - 1][1]) {
+            continue;
         }
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j][0] <= end) {
+                end = Math.max(end, arr[j][1])
+            } else {
+                break;
+            }
+        }
+
+        result.push([start, end])
     }
-    return res;
 
-    // for (let i = 0; i < n; i++) {
-    //     let start = arr[i][0];
-    //     let end = arr[i][1];
-
-    //     if (res.length && end <= res[res.length - 1][1]) {
-    //         continue;
-    //     }
-
-    //     for (let j = i + 1; j < n; j++) {
-    //         if (arr[j][0] <= end) {
-    //             end = Math.max(end, arr[j][1]);
-    //         } else {
-    //             break;
-    //         }
-    //     }
-    //     res.push([start, end]);
-    // }
-
-    // return res;
-
+    return result
 };
