e3c64fd6f79ea22f451d49de04917484bd40b12a
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-17 20:24:00 +0530

    Time: 2 ms (89.36%), Space: 58.5 MB (61.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -7,29 +7,46 @@ var findMedianSortedArrays = function (nums1, nums2) {
     // Brute Froce Merge Sort and then find the median
     let n1 = nums1.length;
     let n2 = nums2.length;
+    let count = 0;
+    let n = (n1 + n2);
+    let ind2 = Math.floor(n / 2);
+    let ind1 = ind2 - 1
 
-    let nums3 = [];
+    let ind1El = -1, ind2El = -1;
     let i = 0, j = 0;
 
     while (i < n1 && j < n2) {
         if (nums1[i] < nums2[j]) {
-            nums3.push(nums1[i++])
+            if (count == ind1) ind1El = nums1[i]
+            if (count == ind2) ind2El = nums1[i]
+            count++;
+            i++;
         } else {
-            nums3.push(nums2[j++])
+            if (count == ind1) ind1El = nums2[j]
+            if (count == ind2) ind2El = nums2[j]
+            count++;
+            j++;
         }
     }
 
-
-    while (i < n1) nums3.push(nums1[i++])
-    while (j < n2) nums3.push(nums2[j++])
-
-
-    let n = n1 + n2;
-
-    if (n % 2 !== 0) {
-        return nums3[Math.floor(n / 2)]
+    while (i < n1) {
+        if (count == ind1) ind1El = nums1[i]
+        if (count == ind2) ind2El = nums1[i]
+        count++;
+        i++;
+    }
+    while (j < n2) {
+        if (count == ind1) ind1El = nums2[j]
+        if (count == ind2) ind2El = nums2[j]
+        count++;
+        j++;
     }
 
-    const median = (nums3[n / 2] + nums3[(n / 2) - 1]) / 2.0
+
+    if (n % 2 !== 0) {
+        return ind2El;
+    }
+
+    const median = (ind1El + ind2El) / 2.0
     return median;
 };
