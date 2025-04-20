d9cd71631ec11dfee6028cf5dc5755aa09831f9d
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-20 09:53:47 +0530

    Time: 3 ms (39.58%), Space: 61.1 MB (72.4%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.js
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.js
@@ -4,26 +4,25 @@
  * @return {number}
  */
 var maxScore = function (cardPoints, k) {
+    let lsum = 0;
+    let rsum = 0;
+    let maxPoints = 0;
+
+    for (let i = 0; i < k; i++) {
+        lsum += cardPoints[i];
+    }
+    maxPoints = lsum;
+
     let l = k - 1;
     let r = cardPoints.length - 1;
 
-    let lSum = 0;
-    let rSum = 0;
-
-    for (let i = 0; i < k; i++) {
-        lSum += cardPoints[i];
-    }
-
-    let maxSum = lSum;
-
     while (l >= 0) {
-        lSum -= cardPoints[l];
-        rSum += cardPoints[r];
+        lsum -= cardPoints[l];
+        rsum += cardPoints[r];
         l--;
         r--;
 
-        maxSum = Math.max(maxSum, lSum + rSum);
+        maxPoints = Math.max(maxPoints, lsum + rsum)
     }
-
-    return maxSum;
+    return maxPoints;
 };
