1ebfa607a64f290113e3c94590b1a806bf065685
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-20 10:14:02 +0530

    Time: 10 ms (38.76%), Space: 59.2 MB (44.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,21 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let maxLength = 0;
     let set = new Set();
-    let n = s.length;
+    let n = s.length - 1;
 
     let l = 0;
     let r = 0;
-    while (r < n) {
-        while (set.has(s[r])) {
-            set.delete(s[l])
-            l++;
-        }
-        set.add(s[r]);
 
+    let maxLength = 0;
+    while (r <= n) {
+        while (set.has(s[r])) {
+            set.delete(s[l]);
+            l++
+        }
+
+        set.add(s[r])
         maxLength = Math.max(maxLength, r - l + 1)
-        r++;
+        r++
     }
 
     return maxLength;
