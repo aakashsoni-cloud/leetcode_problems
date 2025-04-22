d88be7edecdae574da75bdd72f502a0db240e7cd
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-22 19:23:58 +0530

    Time: 26 ms (84%), Space: 65.6 MB (87.5%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.js
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.js
@@ -3,25 +3,27 @@
  * @return {number}
  */
 var totalFruit = function (fruits) {
-    let n = fruits.length - 1;
     let basket = new Map();
+    let n = fruits.length;
     let l = 0;
     let r = 0;
-    let maxItem = 0;
 
-    while (r <= n) {
+    let maxFruits = 0;
+
+    while (r < n) {
         basket.set(fruits[r], (basket.get(fruits[r]) || 0) + 1);
 
         if (basket.size > 2) {
-            basket.set(fruits[l], basket.get(fruits[l]) - 1);
+            basket.set(fruits[l], (basket.get(fruits[l]) - 1));
             if (basket.get(fruits[l]) == 0) {
                 basket.delete(fruits[l]);
             }
             l++;
         }
-
-        maxItem = Math.max(maxItem, r - l + 1);
+        if (basket.size <= 2) {
+            maxFruits = Math.max(maxFruits, r - l + 1)
+        }
         r++;
     }
-    return maxItem
+    return maxFruits;
 };
