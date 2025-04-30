26d4788ea80bc84fd4939c744f51f788ff5857d4
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-04-30 17:39:05 +0530

    Time: 16 ms (91.85%), Space: 56.5 MB (72.36%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -4,42 +4,43 @@
  * @return {string}
  */
 var minWindow = function (s, t) {
-    // let map = new Map(Array.from({ length: 256 }, (_, i) => [String.fromCharCode(97 + i), 0]));
-
-    let map = {};
-
     let n = s.length;
     let m = t.length;
-
-    if (n < m) return "";
-
-    for (let i = 0; i < 256; i++) {
-        map[String.fromCharCode(i)] = (map[String.fromCharCode(i)] || 0);
-    }
+    let hash = {};
 
     let l = 0;
     let r = 0;
 
     let count = 0;
-    let minLength = Number.MAX_SAFE_INTEGER;
+    let minLength = Infinity;
     let sIndex = -1;
 
-    for (let i = 0; i < m; i++) { // o(m)
-        map[t[i]] = (map[t[i]] || 0) + 1
+    for (let i = 0; i < 26; i++) {
+        hash[String.fromCharCode(i)] = hash[String.fromCharCode(i)] || 0;
     }
 
-    while (r < n) { // O(n)
-        if (map[s[r]] && map[s[r]] > 0) count++;
-        map[s[r]] = (map[s[r]] || 0) - 1;
+    for (let i = 0; i < m; i++) {
+        hash[t[i]] = (hash[t[i]] || 0) + 1
+    }
 
+    while (r < n) {
+        if (hash[s[r]] && hash[s[r]] > 0) {
+            count++;
+        }
+
+
+        hash[s[r]] = (hash[s[r]] || 0) - 1
+
+        // Shrink
         while (count == m) {
             if (r - l + 1 < minLength) {
                 minLength = r - l + 1;
                 sIndex = l;
             }
 
-            map[s[l]]++;
-            if (map[s[l]] > 0) {
+            hash[s[l]]++;
+
+            if (hash[s[l]] > 0) {
                 count--;
             }
             l++;
@@ -47,7 +48,5 @@ var minWindow = function (s, t) {
         r++;
     }
 
-    console.log(sIndex, minLength)
-
-    return sIndex == -1 ? "" : s.substring(sIndex, sIndex + minLength)
+    return minLength == Infinity ? "" : s.substring(sIndex, sIndex + minLength)
 };
