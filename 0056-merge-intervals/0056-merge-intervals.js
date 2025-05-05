ee5790fe4cc2cd46d474a8fe1d8920e49d0eb16e
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-05-05 11:49:04 +0530

    Time: 7 ms (86.45%), Space: 64.5 MB (71.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -2,19 +2,19 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function (arr) {
-    let n = arr.length;
-    arr.sort((a, b) => a[0] - b[0]);
-    let result = [];
+var merge = function (intervals) {
+    let n = intervals.length;
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    let res = [];
 
     for (let i = 0; i < n; i++) {
-        if (!result.length || arr[i][0] > result[result.length - 1][1]) {
-            result.push(arr[i]);
+        if (!res.length || intervals[i][0] > res[res.length - 1][1]) {
+            res.push(intervals[i])
+        } else {
+            res[res.length - 1][1] = Math.max(res[res.length - 1][1], intervals[i][1])
         }
-
-        result[result.length - 1][1] = Math.max(result[result.length - 1][1], arr[i][1])
-
     }
 
-    return result
+    return res;
 };
