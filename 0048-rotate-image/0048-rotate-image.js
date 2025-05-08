ee9170f28f9e1158a7bed01fd0b081615228b2ad
Aakash Deep Soni <56084559+aakashsoni-cloud@users.noreply.github.com>
2025-05-08 15:20:28 +0530

    Time: 0 ms (100%), Space: 54.6 MB (32.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,19 +3,26 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function (matrix) {
-
     let n = matrix.length;
     let m = matrix[0].length;
 
-    for (let i = 0; i < n; i++) {
+
+// for sides
+    for (let i = 0; i < n - 1; i++) {
         for (let j = i + 1; j < m; j++) {
-            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
 
+// for mid ones
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < Math.floor(m / 2); j++) {
-            [matrix[i][j], matrix[i][m - 1 - j]] = [matrix[i][m - 1 - j], matrix[i][j]];
+            [matrix[i][j], matrix[i][m - 1 - j]] = [
+                matrix[i][m - 1 - j],
+                matrix[i][j],
+            ];
         }
     }
+
+    return matrix;
 };
